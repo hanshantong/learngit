912272452625b4cec99d2fa24ab832bec953aa85
hanshantong <1356466973@qq.com>
2018-08-07 11:22:23 +0800

    add three java files,modify Cat.java

---
+++ Cat.java
@@ -0,0 +1,16 @@
+package com.tongzi.java
+public class Cat implements Animal {
+	public void sleep() {
+		System.out.println("Cat sleep ...");
+	}
+	public void eat() {
+		System.out.println("Cat eat ...");
+	}
+	public void move() {
+		System.out.println("Cat move ...");
+	}
+	
+	public void yield() {
+		
+	}
+}
