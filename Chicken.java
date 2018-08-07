35c632f5650a01b192b0b9e04e6854b01cecf4e7
hanshantong <1356466973@qq.com>
2018-08-07 14:12:16 +0800

    Implemented three method from interface Animal.

--- Chicken.java
+++ Chicken.java
@@ -1,4 +1,15 @@
 public class Chicken implements Animal {
+	
+	public void sleep() {
+		System.out.println("Chicken sleep ...");
+	}
+	public void eat() {
+		System.out.println("Chicken eat ...");
+	}
+	public void move() {
+		System.out.println("Chicken move ...");
+	}
+	
 	public void crow() {
 		int step = 1;
 		for(int i = 0; i < 1000; ++i) {
