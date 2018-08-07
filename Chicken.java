382ba976debfb47617293c45462403f15a55dace
john <2407410655@qq.com>
2018-08-07 12:34:47 +0800

    Add Chicken.java files...

---
+++ Chicken.java
@@ -0,0 +1,9 @@
+public class Chicken implements Animal {
+	public void crow() {
+		int step = 1;
+		for(int i = 0; i < 1000; ++i) {
+			Thread.sleep(1000);
+			System.out.println("Chicken crow " + i);
+		}
+	}
+}
